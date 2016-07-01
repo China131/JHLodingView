3ab58057ccf401fb16a6f71e8b2ef51653f51f22
China131 <13162255392@163.com>
2016-07-01 14:35:32 +0800

    Initial Commit

---
+++ JHLodingViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JHLodingViewDemo
+//
+//  Created by 简豪 on 16/7/1.
+//  Copyright © 2016年 codingMan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
